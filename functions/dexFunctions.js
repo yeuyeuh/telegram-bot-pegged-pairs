610ecb940861c350668a079c78aa4c703e773c88
yeuyeuh <icerveramarzal@gmail.com>
2023-11-14 17:15:00 +0100

    remove interval

--- functions/dexFunctions.js
+++ functions/dexFunctions.js
@@ -71,7 +71,9 @@ async function send_stETH_WETH_Price() {
   }
 }
 
+/*
 // Déclencher la fonction toutes les minutes
 setInterval(() => {
   send_stETH_WETH_Price();
 }, 10 * 1000); // 1 minute en millisecondes
+*/
